bb79c8f298aceff7da487546ddb00447d874714f
Jason Witt <jawittdesigns@gmail.com>
2018-01-01 12:54:17 -0500

    Added enqueue scripts

---
+++ includes/classes/class-enqueue-scripts.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Enqueue Scripts.
+ *
+ * @package    Theme_Package
+ * @subpackage Theme_Package/Includes/Classes
+ * @author     Theme_Author <Theme_Author_Email>
+ * @copyright  Copyright (c) 2018, Theme_Author
+ * @license    GNU General Public License v2 or later
+ * @version    1.0.0
+ */
+
+namespace Theme_Package\Includes\Classes;
+
+if ( ! class_exists( 'Enqueue_Scripts' ) ) {
+
+	/**
+	 * Enqueue Styles
+	 *
+	 * @author Theme_Author
+	 * @since  1.0.0
+	 */
+	class Enqueue_Scripts extends Enqueue_Abstract {
+
+		/**
+		 * Initialize the class
+		 *
+		 * @author Theme_Author
+		 * @since  1.0.0
+		 *
+		 * @param array $args {
+		 *     Array of enqueued script arguments.
+		 *
+		 *     @type array {
+		 *         Array or string of arguments for the script being registered.
+		 *
+		 *         @type string $handle        (Required) The handle or name of the script.
+		 *         @type string $scr           (Required) The source of the file to enqueue.
+		 *         @type string $dependecies   (Optional) The dependencies of the enqueued file.
+		 *                                         Default: array()
+		 *         @type string $version       (Optional) The version of the file.
+		 *                                         Default: '1.0.0'.
+		 *         @type string $media     (Optional) If Stylesheet, The media for which this stylesheet has been defined.
+		 *                                         Default: 'all'.
+		 *         @type string $in_footer     (Optional) If JavaScript, set to true to enqueue file in the footer.
+		 *                                         Default: true.
+		 *     }
+		 * }
+		 *
+		 * @return void
+		 */
+		public function __construct( $args = array() ) {
+			parent::__construct( $args );
+		}
+
+		/**
+		 * Hooks.
+		 *
+		 * @author Theme_Author
+		 * @since  1.0.0
+		 *
+		 * @return void
+		 */
+		public function hooks() {
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+		}
+
+		/**
+		 * Enqueue Scripts.
+		 *
+		 * @author Theme_Author
+		 * @since  1.0.0
+		 *
+		 * @return void
+		 */
+		public function enqueue() {
+			foreach ( $this->args as $arg ) {
+				wp_enqueue_script( $arg['handle'], $arg['scr'], $arg['dependencies'], $arg['version'], $arg['in_footer'] );
+			}
+		}
+	}
+}
