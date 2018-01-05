6df7c2d9803916d158498bebc6ea6a167f66ee31
Jason Witt <jawittdesigns@gmail.com>
2018-01-04 19:04:46 -0500

    Moved classes to subfolder

--- includes/classes/core/class-template-tags.php
+++ includes/classes/core/class-template-tags.php
@@ -17,16 +17,16 @@ if ( ! class_exists( 'Template_Tags' ) ) {
 	/**
 	 * Load Template Tag Functions.
 	 *
-	 * @author Theme_Author
-	 * @since  1.0.0
+	 * @author Jason Witt
+	 * @since  0.0.1
 	 */
 	class Template_Tags {
 
 		/**
 		 * Directory.
 		 *
-		 * @author Theme_Author
-		 * @since  1.0.0
+		 * @author Jason Witt
+		 * @since  0.0.1
 		 *
 		 * @var string dir The directory that contains the template tag functions.
 		 */
@@ -35,8 +35,8 @@ if ( ! class_exists( 'Template_Tags' ) ) {
 		/**
 		 * Initialize the class
 		 *
-		 * @author Theme_Author
-		 * @since  1.0.0
+		 * @author Jason Witt
+		 * @since  0.0.1
 		 *
 		 * @param string $dir The directory that contains the template tag functions.
 		 *
@@ -44,7 +44,7 @@ if ( ! class_exists( 'Template_Tags' ) ) {
 		 */
 		public function __construct( $dir ) {
 			// Set the directory path.
-			$this->dir = trailingslashit( get_stylesheet_directory() ) . $dir;
+			$this->dir = $dir;
 
 			// Include the files.
 			$this->include_the_files();
@@ -53,17 +53,17 @@ if ( ! class_exists( 'Template_Tags' ) ) {
 		/**
 		 * Initiate.
 		 *
-		 * @author Theme_Author
-		 * @since  1.0.0
+		 * @author Jason Witt
+		 * @since  0.0.1
 		 *
 		 * @return void
 		 */
 		public function include_the_files() {
-			// Get the file to include.
-			$files = $this->scan_directory( $this->dir );
+			// Get the files to include.
+			$files = $this->scan_directory();
 
-			// Loop through the files array.
-			foreach ( $files as $file ) {
+			// Loop through the $files array.
+			foreach( $files as $file ) {
 
 				// Verify that the file exists.
 				if ( file_exists( $file ) ) {
@@ -77,8 +77,8 @@ if ( ! class_exists( 'Template_Tags' ) ) {
 		/**
 		 * Scan the Directory.
 		 *
-		 * @author Theme_Author
-		 * @since  1.0.0
+		 * @author Jason Witt
+		 * @since  0.0.1
 		 *
 		 * @return array
 		 */
@@ -86,18 +86,21 @@ if ( ! class_exists( 'Template_Tags' ) ) {
 			// Declare the results array to return.
 			$results = array();
 
-			// Scan the dir for files.
-			$files = scandir( $this->dir );
+			// Recursively scan the dirs for files.
+			$files = new \RecursiveDirectoryIterator( $this->dir );
 
-			// Loop through the files in the directory.
-			foreach ( $files as $file ) {
-				// Exclude . and ..
-				if ( ( '.' === $file ) || ( '..' === $file ) ) {
+			// Loop through the files.
+			foreach ( new \RecursiveIteratorIterator( $files ) as $file ) {
+				$filename = $file->getFilename();
+				$filepath = $file->getPathname();
+				
+				// Exclude dot files.
+				if ( '.' === substr( $filename, 0, 1 ) ) {
 					continue;
 				}
 
 				// Get the path to the file.
-				$file = trailingslashit( $this->dir ) . $file;
+				$file = $filepath;
 
 				// Get the file header info.
 				$header = get_file_data( $file, array( 'Load' => 'Load' ) );
