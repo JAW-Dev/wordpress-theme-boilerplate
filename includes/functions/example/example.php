16e346fb52115501c6e117f686e3e4286d1b490c
Jason Witt <jawittdesigns@gmail.com>
2018-01-04 19:05:11 -0500

    Added example template tag file

---
+++ includes/functions/example/example.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Example Function.
+ *
+ * Load: true
+ *
+ * @package    Theme_Package
+ * @subpackage Theme_Package/Includes/Classes
+ * @author     Theme_Author <Theme_Author_Email>
+ * @copyright  Copyright (c) 2018, Theme_Author
+ * @license    GNU General Public License v2 or later
+ * @version    1.0.0
+ */
+
+if ( ! function_exists( 'Theme_Textdomain_example' ) ) {
+	/**
+	 * Example Function.
+	 *
+	 * @author Jason Witt
+	 * @since  1.0.0
+	 *
+	 * @return string
+	 */
+	function Theme_Textdomain_example() {
+		return 'This is an example function template tag';
+	}
+}
