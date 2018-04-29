0a1d3442dc4b159e4b023cf97c45edb3dfd9997b
Jason Witt <jawittdesigns@gmail.com>
2018-04-29 11:10:36 -0400

    Removed get_post_navigation()

--- search.php
+++ search.php
@@ -28,7 +28,6 @@ if ( have_posts() ) :
 	while ( have_posts() ) :
 		the_post();
 		get_template_part( 'template-parts/content', 'search' );
-		the_post_navigation();
 		if ( comments_open() || get_comments_number() ) :
 			comments_template();
 		endif;
--- templates/template-page.php
+++ templates/template-page.php
@@ -18,7 +18,6 @@ get_header();
 while ( have_posts() ) :
 	the_post();
 	get_template_part( 'template-parts/content', 'page' );
-	the_post_navigation();
 	if ( comments_open() || get_comments_number() ) :
 		comments_template();
 	endif;
